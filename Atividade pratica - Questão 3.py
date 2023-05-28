cbd026dff63b42b8923493034a21e7a8297b210d
rnb183 <rodrigo.nucci@dilettasolutions.com>
2023-05-28 01:07:39 -0300

    exercicio 4

--- Atividade pratica - Questão 3.py
+++ Atividade pratica - Questão 3.py
@@ -4,12 +4,14 @@ RU = 4473087
 def dimensoesObjeto():
     while True:
         try:
+            # Solicita as dimensões do objeto
             altura = float(input('Digite a altura do Objeto em cm '))
             comprimento = float(input('Digite o comprimento do Objeto em cm '))
             largura = float(input('Digite a largura do objeto em cm '))
 
-            volume = altura * comprimento * largura
+            volume = altura * comprimento * largura  # Calcula o volume do objeto
 
+            # Define o valor com base no volume
             if volume < 1000:
                 valor = 10
             elif 1000 <= volume < 10000:
@@ -19,21 +21,27 @@ def dimensoesObjeto():
             elif 30000 <= volume < 100000:
                 valor = 50
             else:
+                # Se o volume for muito alto, imprime uma mensagem de erro e retorna ao início do loop
                 print('O volume desse objeto é muito alto para o nosso transporte')
                 print('Entre novamente com as dimensões')
                 continue
 
+            # Imprime o volume e o preço para o objeto
             print('O volume desse objeto é {:.2f} cm³'.format(volume))
             print('O preço para esse objeto é R${:.2f}'.format((valor)))
             return valor
         except ValueError:
+            # Se um valor inválido for digitado, imprime uma mensagem de erro e retorna ao início do loop
             print('Digite um valor valido para as dimensões do objeto')
 
 def pesoObjeto():
     multiplicador = 0
     while True:
         try:
+            # Solicita o peso do objeto
             peso = float(input('Digite o peso do objeto em Kg '))
+
+            # Define o multiplicador com base no peso
             if peso <= 0.1:
                 multiplicador = 1
             elif 0.1 <= peso < 1:
@@ -43,13 +51,16 @@ def pesoObjeto():
             elif 10 <= peso < 30:
                 multiplicador = 3
             else:
+                # Se o peso for muito alto, imprime uma mensagem de erro e retorna ao início do loop
                 print('Objeto muito pesado')
                 print('Entre novamente com o peso do objeto')
                 continue
             break
         except ValueError:
+            # Se um valor inválido for digitado, imprime uma mensagem de erro e retorna ao início do loop
             print('Valor inválido, tente novamente')
 
+    # Imprime o peso e o multiplicador para o objeto
     print('O peso do seu objeto é {:.2f}Kg'.format(peso))
     print('O multiplicador para esse peso é {}'.format(multiplicador))
     return multiplicador
@@ -58,8 +69,10 @@ def rotaObjeto():
     multiplicador = 0
     desenhar_rotas()
     while True:
+        # Solicita a sigla da rota desejada
         rota = input('Insira a sigla da rota desejada ').upper()
 
+        # Define o multiplicador com base na rota
         if rota == 'RS' or rota == 'SR':
             multiplicador = 1
         elif rota == 'BS' or rota == 'SB':
@@ -67,31 +80,35 @@ def rotaObjeto():
         elif rota == 'BR' or rota == 'RB':
             multiplicador = 1.5
         else:
+            # Se uma sigla inválida for digitada, imprime uma mensagem de erro e retorna ao início do loop
             print('Insira uma sigla valida')
             continue
         break
 
-
+    # Imprime a rota selecionada e o multiplicador
     print('A rota selecionada foi {} com multiplicador {}'.format(rota, multiplicador))
     return multiplicador
-
 
 def desenhar_rotas():
     largura_sigla = 10
     largura_descricao = 32
     largura_multiplocador = 14
     largura_total = largura_multiplocador + largura_descricao + largura_sigla
+
+    # Imprime o cabeçalho das rotas
     meia_largura = largura_total/2
     print('*' * int(meia_largura) + 'Rotas' + '*' * int(meia_largura))
     print(f'|{"sigla":<{largura_sigla}}|{"Rota":<{largura_descricao}}|{"Multiplicador":<{largura_multiplocador}}|')
+
+    # Imprime as informações de cada rota
     for sigla, (descricao, multiplicador) in rotas.items():
         print(f'|{sigla:<{largura_sigla}}|{descricao:<{largura_descricao}}|{multiplicador:<{largura_multiplocador}}|')
+
     print('*' * (largura_total + 5))
 
+# Programa Principal
 
-
-#Programa Principal
-
+# Dicionário das rotas com suas descrições e multiplicadores
 rotas = {'RS': ['De Rio de Janeiro até São Paulo', 1],
          'SR': ['De São Paulo até Rio de Janeiro', 1],
          'BS': ['De Brasília até São Paulo', 1.2],
@@ -101,13 +118,21 @@ rotas = {'RS': ['De Rio de Janeiro até São Paulo', 1],
          }
 
 print('_' * 60)
+
+# Solicita e calcula as dimensões do objeto
 dimensoes = dimensoesObjeto()
 print('_' * 60)
+
+# Solicita e calcula o peso do objeto
 peso = pesoObjeto()
 print('_' * 60)
+
+# Solicita e define a rota do objeto
 rota = rotaObjeto()
 print('_' * 60)
 
+# Calcula o valor final com base nas dimensões, peso e rota
 valor_final = dimensoes * peso * rota
 
+# Imprime o valor final a ser pago, juntamente com as informações do objeto
 print('O total a pagar é: R${} (Volume: {}cm³ * Multiplicador do Peso: {} * Multiplicador da Rota: {})'.format(valor_final, dimensoes, peso, rota))
---
+++ Atividade pratica - Questão 4.py
@@ -0,0 +1,121 @@
+print('Bem-vindo(a) ao Controle de Estoque da Bicicletaria do Rodrigo Nucci Bragalia')
+RU = 4473087
+
+def cadastrarPeca():
+    # Função para cadastrar uma nova peça no estoque
+    peca = {
+        'nome': input('Insira o nome da peça a ser cadastrada: '),
+        'fabricante': input('Insira o fabricante da peça: '),
+        'valor': float(input('Insira o valor da peça: ')),
+    }
+    return peca
+
+
+def consultarPeca(pecas):
+    # Função para consultar peças no estoque
+    while True:
+        print('\n*** Consultar Peças ***')
+        print('1. Visualizar todas as peças cadastradas')
+        print('2. Pesquisar peças por código')
+        print('3. Pesquisar peças por fabricante')
+        print('4. Voltar')
+
+        opcao = input('Escolha uma opção de acordo com o menu acima: ')
+
+        if opcao == '1':
+            consultarTodasPecas(pecas)
+        elif opcao == '2':
+            consultarPecasPorCodigo(pecas)
+        elif opcao == '3':
+            consultarPecasPorFabricante(pecas)
+        elif opcao == '4':
+            break
+        else:
+            print('Opção inválida! Por favor, escolha uma opção válida.')
+
+
+def consultarTodasPecas(pecas):
+    # Função para exibir todas as peças cadastradas no estoque
+    print('\n*** Consultar Todas as Peças Cadastradas ***')
+    if pecas:
+        for i in range(len(pecas)):
+            peca = pecas[i]
+            print(f'Peça {i+1}:')
+            print(f'Nome: {peca["nome"]}')
+            print(f'Fabricante: {peca["fabricante"]}')
+            print(f'Valor: R$ {peca["valor"]:.2f}')
+            print('*' * 60)
+    else:
+        print('Não há peças cadastradas no estoque.')
+
+
+def consultarPecasPorCodigo(pecas):
+    # Função para pesquisar peças por código no estoque
+    print('\n*** Consultar Peças por Código ***')
+    codigo = input('\nDigite o código da peça desejada: ')
+    for peca in pecas:
+        if peca['codigo'] == codigo:
+            print(f'Código: {peca["codigo"]}')
+            print(f'Nome: {peca["nome"]}')
+            print(f'Fabricante: {peca["fabricante"]}')
+            print(f'Valor: R$ {peca["valor"]:.2f}')
+            break
+    else:
+        print('Não existe peça cadastrada com esse código')
+
+
+def consultarPecasPorFabricante(pecas):
+    # Função para pesquisar peças por fabricante no estoque
+    print('\n*** Consultar Peças Por Fabricante ***')
+    fabricante = input('\nDigite o fabricante da peça: ')
+    for peca in pecas:
+        if peca['fabricante'] == fabricante:
+            print(f'Código: {peca["codigo"]}')
+            print(f'Nome: {peca["nome"]}')
+            print(f'Fabricante: {peca["fabricante"]}')
+            print(f'Valor: R$ {peca["valor"]:.2f}')
+            print('*' * 60)
+
+
+def removerPeca(pecas):
+    # Função para remover uma peça do estoque
+    codigo = input('Insira o código da peça que você deseja excluir: ')
+    for i in range(len(pecas)):
+        if pecas[i]['codigo'] == codigo:
+            del pecas[i]
+            print('Peça excluída com sucesso do estoque.')
+            break
+    else:
+        print('Peça não localizada.')
+
+
+pecas = []  # Lista para armazenar as peças do estoque
+contador = 1  # Variável para controlar o código das peças
+
+while True:
+    print('\n--- Menu ---')
+    print('1. Adicionar Peça ao Estoque')
+    print('2. Consultar Peças no Estoque')
+    print('3. Remover Peça do Estoque')
+    print('4. Sair')
+
+    opcao = input('Digite o número da opção desejada: ')
+
+    if opcao == '1':
+        print('\n--- Adicionar Peça ao Estoque ---')
+        peca = cadastrarPeca()
+        peca['codigo'] = str(contador)  # Atribui um código à peça
+        pecas.append(peca)  # Adiciona a peça à lista de peças
+        contador += 1  # Incrementa o contador de código
+        print('Peça cadastrada com sucesso no estoque.')
+    elif opcao == '2':
+        consultarPeca(pecas)
+    elif opcao == '3':
+        print('\n--- Remover Peça do Estoque ---')
+        removerPeca(pecas)
+    elif opcao == '4':
+        break
+    else:
+        print('Opção inválida! Por favor, escolha uma opção válida.')
+
+print('Fim do programa.')
